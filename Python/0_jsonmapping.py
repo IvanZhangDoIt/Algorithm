540a94d120fa50062558027e1b4ac0348ffe2896
Ivan <ivanzhangdoit@gmail.com>
2019-12-20 15:03:09 +1300

    update python for hierachy issue

--- Python/0_jsonmapping.py
+++ Python/0_jsonmapping.py
@@ -17,18 +17,26 @@ def rule_converter(input: dict, mapping_rules: dict):
 
         if (splitter in output_attribute):
             output_attribute_chain = output_attribute.split(splitter)
-            obj = value
-            for attr in output_attribute_chain[::-1]:
-                temp = {}
-                temp[attr] = obj
-                obj = temp.copy()
-            result.update(temp)
+            obj = result
+            length = len(output_attribute_chain)
+            for i in range(length):
+                key = output_attribute_chain[i]
+                if key not in obj.keys():
+                    obj[key] = value if (i==length-1) else {}
+                obj = obj[key]
         else:
             result[output_attribute] = value
 
         print("input_attribute is {}, output_attribute is {}".format(input_attribute, output_attribute))
 
-    return json.dumps(result, sort_keys=True)
+    return result
+
+def test(input, mapping_rules, expected):
+    print("\nThe input is :\n{}".format(input))
+    print("\nThe mapping rule is:\n{}".format(mapping_rules))
+    output = rule_converter(input, mapping_rules)
+    print("The ouput is : \n{}".format(output))
+    print("-------------------------------------------\nTest result is: {} \n -----------------------------------------".format(json.dumps(expected) == json.dumps(output)))
 
 if __name__ == "__main__":
     input = {
@@ -44,8 +52,41 @@ if __name__ == "__main__":
         'i_b': 'o_b.o_b_a',
         'i_c': 'o_c'
     }
+    expected =     {
+        "o_a": 12,
+        "o_b": {
+        "o_b_a": "abc"
+        },
+        "o_c": "value of i_c"
+    }
+    test(input,mapping_rules, expected)
 
-    print("\nThe input is :\n{}".format(input))
-    print("\nThe mapping rule is:\n{}".format(mapping_rules))
-    output = rule_converter(input, mapping_rules)
-    print("\nThe ouput is :\n{}".format(output))
+    input = {
+        'i_a': {
+            'i_a_a': 'value of i_a_a',
+            'i_a_b': 'value of i_a_b',
+            'i_a_c': 'value of i_a_c',
+        },
+        'i_b': 'abc',
+        'i_c': 'value of i_c',
+        'i_d': 'value of i_d',
+    }
+    mapping_rules = {
+        'i_a.i_a_a': 'o_a.o_a_a',
+        'i_a.i_a_b': 'o_a.o_a_b',
+        'i_a.i_a_c': 'o_a.o_a_c',
+        'i_b': 'o_b',
+        'i_c': 'o_c',
+        'i_d': 'o_d',
+    }
+    expected =   {
+        "o_a": {
+        "o_a_a": "value of i_a_a",
+        "o_a_b": "value of i_a_b",
+        "o_a_c": "value of i_a_c"
+        },
+        "o_b": "abc",
+        "o_c": "value of i_c",
+        "o_d": "value of i_d"
+    }
+    test(input,mapping_rules, expected)
