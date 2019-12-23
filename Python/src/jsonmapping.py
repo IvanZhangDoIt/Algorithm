4fbebc1f3ecc79f04b30fbc9cc3e0230e31b7293
Ivan <ivanzhangdoit@gmail.com>
2019-12-23 16:07:32 +1300

    clean python code

--- Python/src/jsonmapping.py
+++ Python/src/jsonmapping.py
@@ -6,29 +6,32 @@ def rule_converter(input: dict, mapping_rules: dict):
     for (input_attribute, output_attribute) in mapping_rules.items():
         value = 'Not Assigned'
         if(splitter in input_attribute):
+            # with splitter, it's a layered value.
             input_attributes_chain = input_attribute.split(splitter)
             obj = input
+            # get the deepest attribute
             for attr in input_attributes_chain:
                 obj = obj[attr]
             value = obj
         else: 
+            # splitter not found, get the value directly
             value = input[input_attribute]
         print("\nThe input value is : {}".format(value))
 
         if (splitter in output_attribute):
+            # with splitter, it's a layered value.
             output_attribute_chain = output_attribute.split(splitter)
             obj = result
             length = len(output_attribute_chain)
             for i in range(length):
                 key = output_attribute_chain[i]
                 if key not in obj.keys():
+                    # key not exist, create a new object or set it to the value as it's in the deepest layer.
                     obj[key] = value if (i==length-1) else {}
                 obj = obj[key]
         else:
+            # output attribute has no splitter, set the value directly
             result[output_attribute] = value
-
-        print("input_attribute is {}, output_attribute is {}".format(input_attribute, output_attribute))
-
     return result
 
 if __name__ == "__main__":
